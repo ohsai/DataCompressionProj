e38e046d8eb8cd522e09b2f61d3150b97792f604
ohsai <ohsai@snu.ac.kr>
2020-11-28 22:35:34 +0900

    zip on Benchmark

--- benchmark.py
+++ benchmark.py
@@ -4,9 +4,13 @@ data = ['dnact','englishct','xmlct','SD1.txt','SD2.txt','SD3.txt','SD4.txt']
 tun_best = [4,7,7,8,6,4,3]
 datadir = './data/'
 zipdir = './zip/'
+os.system("rm -rf " + zipdir)
 for algo in comp_algos :
+    os.system("mkdir -p " + zipdir + algo + "/")
     for idx, dataset in enumerate(data) :
         datapath = datadir+ dataset
+        encpath = datadir + dataset + "." + algo + ".enc"
+        zippath = zipdir + algo + "/" + dataset + ".enc"
         if algo == 'tun' :
             for bits in range(3,9) :
                 cmd ="./main "+algo+" "+datapath + " --num_bits " + str(bits)
@@ -14,12 +18,12 @@ for algo in comp_algos :
                 print(cmd)
                 os.system( cmd);
                 if bits == tun_best[idx] : 
-                    #gzip
-                    #bzip
+                    os.system("gzip -c " + encpath + " > " + zippath + ".gz");
+                    os.system("bzip2 -c " + encpath + " > " + zippath + ".bz2");
         else :
             cmd = "./main "+algo+" "+ datapath;
             print("-----------------")
             print(cmd)
             os.system(cmd);
-            #gzip
-            #bzip
+            os.system("gzip -c " + encpath + " > " + zippath + ".gz");
+            os.system("bzip2 -c " + encpath + " > " + zippath + ".bz2");
